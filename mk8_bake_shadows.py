2c9120467d32405d667728c39a1a8b233751e9ab
Scutlet <103026559+Scutlet@users.noreply.github.com>
2022-04-14 15:17:47 +0200

    Bake shadows + combine as channels
    Both Ambient Occlusion and Shadows are now baked. They're combined in the red and green channel respectively.

--- mk8_bake_shadows.py
+++ mk8_bake_shadows.py
@@ -2,6 +2,7 @@ import bpy
 
 from bpy.types import Operator, Panel, PropertyGroup
 from bpy.props import StringProperty, BoolProperty, IntProperty, CollectionProperty, BoolVectorProperty, PointerProperty
+from datetime import datetime
 
 bl_info = {
     "name": "Mario Kart 8 - Bake Shadows",
@@ -20,7 +21,9 @@ bl_info = {
 
 class MK8BakeShadowsProp(PropertyGroup):
     """ Properties for MK8 Bake Shadows """
-    float = BoolProperty(name="32 bit Float")
+    image_name = StringProperty(name="Image Name",
+        description="Name of the generated image. It will be replaced if it already exists."
+    )
 
     res_x = IntProperty (name="X",
         description="X Resolution of the image",
@@ -44,8 +47,8 @@ class MK8BakeShadowsProp(PropertyGroup):
         subtype='PIXEL'
     )
 
-    bake_shadows = BoolProperty(name="Bake Shadows (Red Channel)", default=True)
-    bake_ao = BoolProperty(name="Bake Ambient Occlusion (Green Channel)", default=True)
+    bake_ao = BoolProperty(name="Bake Ambient Occlusion (Red Channel)", default=True)
+    bake_shadows = BoolProperty(name="Bake Shadows (Green Channel)", default=True)
 
 
 class MK8BakeShadowsPanel(bpy.types.Panel):
@@ -66,14 +69,13 @@ class MK8BakeShadowsPanel(bpy.types.Panel):
         col.label(text="Image Resolution:")
         col.prop(prop, "res_x")
         col.prop(prop, "res_y")
-        col.prop(prop, "float")
 
         # Bake Options
         row = layout.row()
         col = row.column(align=True)
         col.label(text="Bake Options:")
-        col.prop(prop, "bake_shadows")
         col.prop(prop, "bake_ao")
+        col.prop(prop, "bake_shadows")
 
         # # UV Override
         # split = layout.split(percentage=0.3)
@@ -92,31 +94,62 @@ class MK8BakeShadows(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
 
     def execute(self, context):
+        print("====================\nMK8 Bake Shadows was started!")
         scene = context.scene
 
         res_x = scene.mk8bakeshadowsprop.res_x
         res_y = scene.mk8bakeshadowsprop.res_y
-        float = scene.mk8bakeshadowsprop.float
 
 
         # bpy.ops.object.bake(type="AO", uv_layer=0)
 
-        image_name = "MK8 Shadows"
+        dt_now = datetime.now()
+
+        image_name = "MK8_Bake1" #+ dt_now.isoformat()
         uv_index = 1
 
-        # Find bake image
-        img = next((img for img in bpy.data.images if img.name == image_name), None)
-        if img is None:
-            # Create a new image if existing one doesn't exist
-            img = bpy.ops.image.new(name=image_name, width=1024, height=1024, alpha=False)
 
-        # Set bake info
-        context.scene.render.bake_type = "AO"
-        context.scene.render.use_bake_normalize = True
 
-        # Exit edit mode
+
+
+        # # Set bake info
+        # context.scene.render.bake_type = "AO"
+        # context.scene.render.use_bake_normalize = True
+        # print(context.scene.render.bake_samples) # for AO (default 256, [64, 1024])
+
+        # More on light shadows
+        # https://docs.blender.org/manual/en/2.79/render/blender_render/lighting/shadows/raytraced_properties.html
+        # Shadows should be enabled; constant falloff; more samples ==> better quality; constant > adaptive; soft-size = shadow-gradient size form light to dark
+
+        # Enter object mode
         bpy.ops.object.mode_set(mode='OBJECT')
 
+        # Bake AO
+        img_ao = self.generate_image(context, image_name + "_ao")
+        self.select_image_and_uv(context, img_ao, uv_index)
+        self.bake(context, bake_type="AO", use_bake_normalize=True)
+
+        # Bake Shadows
+        img_shadows = self.generate_image(context, image_name + "_shadow")
+        self.select_image_and_uv(context, img_shadows, uv_index)
+        self.bake(context, bake_type="SHADOW")
+
+        # Combine bake maps in relevant image channels
+        img_combined = self.generate_image(context, image_name)
+        self.combine_channels(img_combined, red=img_ao, green=img_shadows, base_red=1.0, base_green=0.745, base_blue=0.0, base_alpha=1.0)
+
+        print("MK8 Bake Shadows has finished!\n====================")
+        return {'FINISHED'}
+
+    def generate_image(self, context, image_name):
+        """ TODO """
+        if image_name not in bpy.data.images:
+            # Create a new image if existing one doesn't exist
+            bpy.ops.image.new(name=image_name, width=1024, height=1024, alpha=False)
+        return bpy.data.images[image_name]
+
+    def select_image_and_uv(self, context, img, uv_index):
+        """ TODO """
         # Iterate over all selected objects
         for obj in context.selected_objects:
             if obj.type != 'MESH':
@@ -129,21 +162,75 @@ class MK8BakeShadows(bpy.types.Operator):
                 raise IndexError("%s did not have a UVMap at index %s. Unselect it or add a UV map there" % (obj.name, uv_index))
 
             # Make bake UVs active
-            obj.data.uv_textures[1].active = True
+            obj.data.uv_textures[uv_index].active = True
 
             # Set the target image
-            for d in obj.data.uv_textures[1].data:
-                d.image = bpy.data.images[image_name]
+            for d in obj.data.uv_textures[uv_index].data:
+                d.image = bpy.data.images[img.name]
+
+    def bake(self, context, **kwargs):
+        """ TODO """
+        render = context.scene.render
+
+        # Set render settings & store old ones
+        old_settings = {}
+        for setting, val in kwargs.items():
+            old_settings[setting] = getattr(render, setting)
+            setattr(render, setting, val)
 
         # Bake the image
+        print("Performing %s bake with settings: %s" % (render.bake_type, kwargs))
         bpy.ops.object.bake_image()
 
-        return {'FINISHED'}
+        # Cleanup: Restore old render settings
+        for setting, val in old_settings.items():
+            setattr(render, setting, val)
 
+    def combine_channels(self, img, red=None, green=None, blue=None, alpha=None,
+            base_red=1.0, base_green=1.0, base_blue=1.0, base_alpha=1.0):
+        """ TODO """
+        print("Writing combined channel...")
+
+        # Image editing is slow, so we create a copy of all the pixels first: https://blender.stackexchange.com/a/3678
+        #   Using the tuple object is way faster than direct access to Image.pixels
+        pixels = list(img.pixels)
+        red_channel = red and list(red.pixels)
+        green_channel = green and list(green.pixels)
+        blue_channel = blue and list(blue.pixels)
+        alpha_channel = alpha and list(alpha.pixels)
+
+        # Red Channel
+        print("\tWriting red channel...")
+        for i in range(0, len(pixels), 4):
+            pixels[i] = red_channel[i]
+
+        # Green Channel
+        print("\tWriting green channel...")
+        for i in range(1, len(pixels), 4):
+            pixels[i] = green_channel[i]
+
+        # Blue Channel
+        print("\tWriting blue channel...")
+        for i in range(2, len(pixels), 4):
+            pixels[i] = base_blue
+
+        # Alpha Channel
+        print("\tWriting alpha channel...")
+        for i in range(3, len(pixels), 4):
+            pixels[i] = base_alpha
+
+        # Write back to image (Slice notation here means to replace in-place, not sure if it's faster...)
+        img.pixels[:] = pixels
+
+        # Should probably update image
+        img.update()
 
 def register():
     """ Runs when enabling the addon """
     print("Enabled MK8 Bake Shadows!")
+    import sys
+    print(sys.version)
+
     bpy.utils.register_class(MK8BakeShadows)
     bpy.utils.register_class(MK8BakeShadowsPanel)
     bpy.utils.register_class(MK8BakeShadowsProp)
